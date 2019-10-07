a08b8d13cce010ff8bc705a4256a699c075c5a24
Tal Weiss <Tweiss747@gmail.com>
2019-10-06 23:19:01 -0400

    creating login form

--- todoListFrontEnd/src/app/app.module.ts
+++ todoListFrontEnd/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   // this is where your components get registered
+   declarations: [
+      AppComponent,
+      LoginComponent,
+      
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
