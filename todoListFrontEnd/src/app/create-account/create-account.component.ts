34f5810b9b75da65ed28ff11a7c2a9442d643d42
Tal Weiss <Tweiss747@gmail.com>
2019-10-09 23:49:52 -0400

    forms work adding routing

--- todoListFrontEnd/src/app/create-account/create-account.component.ts
+++ todoListFrontEnd/src/app/create-account/create-account.component.ts
@@ -7,10 +7,25 @@ import { Account } from './../models/Account';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
+  
+  newAccount: Account = {
+    first_name: '',
+    last_name: '', 
+    email: '',
+    password: '',
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // used for debugging purposes
+  printToConsole(): void{
+    console.log(this.newAccount.first_name)
+    console.log(this.newAccount.last_name)
+    console.log(this.newAccount.email)
+    console.log(this.newAccount.password)
   }
 
 }
