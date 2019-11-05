02405387a68386ca2fb2b1b8919b36ef0ab14200
Tal Weiss <Tweiss747@gmail.com>
2019-11-05 16:57:28 -0500

    added form validation to the login component and disabled buttons until the forms are filled

--- todoListFrontEnd/src/app/login/login.component.ts
+++ todoListFrontEnd/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from './../models/User';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
   // Email and password fields
   user: User = {
     email: '',
@@ -19,12 +21,27 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    this.loginForm = new FormGroup({
+      'email': new FormControl(this.user.email,[Validators.required,Validators.pattern('^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})')]),
+      'password': new FormControl(this.user.password,[Validators.required])
+    });
   }
 
+  //#region getters
+  get email(){
+    return this.loginForm.get('email');
+  }
+  
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  //#endregion
+
   // login button
-  loginBtn():void{
+  login():void{
     // send username and password into a post request to the server.
+    console.log("Login button pressed");
 
   }
   
