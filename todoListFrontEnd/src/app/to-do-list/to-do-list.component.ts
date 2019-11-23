103c04b79a3b3982bc26c7bcba88a787b944c4be
Tal Weiss <Tweiss747@gmail.com>
2019-11-23 16:43:59 -0500

    styling todolist, adding login functions, and added a service to handle todo list items

--- todoListFrontEnd/src/app/to-do-list/to-do-list.component.ts
+++ todoListFrontEnd/src/app/to-do-list/to-do-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserService} from '../user-service.service'
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(userService: UserService) { }
+  todolist: any = [];
   ngOnInit() {
+    this.todolist = this.retrievelist();
   }
 
+edit(){
+  console.log('edit button pressed');
+  // call function from user services
 }
+
+
+delete(){
+  console.log('delete button pressed');
+// call function from user services
+}
+
+create_todoItem(){
+  console.log('create item button pressed');
+  // call function from user services
+}
+
+// should be called in nginit
+retrievelist(){
+  console.log("retrieve list triggered")
+}
+
+}
+
+
+
+
+
